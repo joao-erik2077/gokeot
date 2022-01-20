bc9dbae51afef261129e2d704b145c57ebbe77ae
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-01-20 12:47:32 -0300

    Adding welcome message

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client(
     {
         intents: [
             "GUILDS",
-            "GUILD_MESSAGES"
+            "GUILD_MESSAGES",
+            "GUILD_MEMBERS"
         ]
     }
 )
+
+
+const welcomeChatId = "933731933739958303"
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChatId).send({
+        content: `Seja bem vindo <@${member.id}>!!`,
+        files: [img]
+    })
+})
 
 client.on("ready", () => {
     console.log(`Logado como ${client.user.tag}`)
