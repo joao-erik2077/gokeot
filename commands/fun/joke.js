b3f4463560cadde6513bb35c01713a09b50e7033
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-01-20 21:34:59 -0300

    adding commands

---
+++ commands/fun/joke.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "joke",
+    category: "fun",
+    permissions: [],
+    devOnly: false,
+    run: async ({client, message, args}) => {
+        message.reply("Porque a galinha atravessou a rua? Para chegar ao outro lado")
+    }
+}
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
-const generateImage = require("./generateImage")
+// const generateImage = require("./generateImage")
 
 const client = new Discord.Client(
     {
@@ -12,25 +12,41 @@ const client = new Discord.Client(
         ]
     }
 )
+ 
+let bot = {
+    client,
+    prefix: "gk!",
+    owners: ["516671423301812224"]
+}
 
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
 
-const welcomeChatId = "933731933739958303"
-client.on("guildMemberAdd", async (member) => {
-    const img = await generateImage(member)
-    member.guild.channels.cache.get(welcomeChatId).send({
-        content: `Seja bem vindo <@${member.id}>!!`,
-        files: [img]
-    })
-})
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
+client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
 
-client.on("ready", () => {
-    console.log(`Logado como ${client.user.tag}`)
-})
+client.loadEvents(bot, false)
+client.loadCommands(bot, false)
 
-client.on("messageCreate", (message) => {
-    if (message.content == "Oi") {
-        message.reply("Olá")
-    }
-})
+module.exports = bot
+
+// const welcomeChatId = "933731933739958303"
+// client.on("guildMemberAdd", async (member) => {
+//     const img = await generateImage(member)
+//     member.guild.channels.cache.get(welcomeChatId).send({
+//         content: `Seja bem vindo <@${member.id}>!!`,
+//         files: [img]
+//     })
+// })
+
+// client.on("ready", () => {
+//     console.log(`Logado como ${client.user.tag}`)
+// })
+
+// client.on("messageCreate", (message) => {
+//     if (message.content == "Oi") {
+//         message.reply("Olá")
+//     }
+// })
 
 client.login(process.env.TOKEN)
