0b81e2d94f4c0ed602f424a491511cfdf5af5f07
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-01-23 20:22:03 -0300

    clear command added

---
+++ commands/utility/clear.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "clear",
+    category: "utility",
+    permissions: ["934637943702835310"],
+    devOnly: false,
+    run: async ({client, message, args}) => {
+        if (!args[0])
+            return message.reply("Digite com a quantidade de mensagens a serem apagadas")
+        if (isNaN(args[0]))
+            return message.reply("Digite um número verdadeiro")
+        if (args[0] > 100 || args[0] < 1)
+            return message.reply("Digite um valor entre 1 e 100")
+        
+        await message.channel.messages.fetch({limit: args[0]}).then(messages => {
+            message.channel.bulkDelete(messages)
+        })
+    }
+}
