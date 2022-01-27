3f41c5383b6164136a180347f7dc14f1cffcaf60
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-01-26 22:25:48 -0300

    image command added

---
+++ commands/info/image.js
@@ -0,0 +1,21 @@
+var Scraper = require('images-scraper')
+const google = new Scraper({
+    puppeteer: {
+      headless: true
+    }
+})
+
+module.exports = {
+    name: "image",
+    category: "info",
+    permissions: [],
+    devOnly: false,
+    run: async ({client, message, args}) => {
+        const image_query = args.join(" ")
+        if (!image_query) 
+            return message.reply("Digite o nome da imagem")
+        
+        const image_results = await google.scrape(image_query, 1)
+        message.reply(image_results[0].url)
+    }
+}
