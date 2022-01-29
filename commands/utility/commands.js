7632da2293782ec3de102930004688002a1c53bf
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-01-28 23:14:38 -0300

    changes in commands display

--- commands/utility/commands.js
+++ commands/utility/commands.js
@@ -1,20 +1,27 @@
 const { getFiles } = require("../../util/functions")
 const fs = require("fs")
+const { MessageEmbed, Channel } = require("discord.js")
 
 module.exports = {
     name: "commands",
     category: "utility",
     permissions: [],
     devOnly: false,
-    run: async ({client, message, args}) => {
-        let msg = 'Lembre-se de usar o prefixo "gk!" antes de cada comando!\n\n'
-        fs.readdirSync("./commands/").forEach((category) => {
-            let commands = getFiles(`./commands/${category}`, ".js")
-    
-            commands.forEach((f) => {
-                msg += `${f.split(".", 1)}\n`
+
+    run: async({client, message, args}) => {
+        const commandsEmbed = new MessageEmbed()
+            .setColor("#0099ff")
+            .setThumbnail("https://cdn-icons-png.flaticon.com/512/0/656.png")
+
+            let msg = ""
+            fs.readdirSync("./commands/").forEach((category) => {
+                let commands = getFiles(`./commands/${category}`, ".js")
+                
+                commands.forEach((f) => {
+                    msg += `${f.split(".", 1)}\n`
+                })
             })
-        })
-        message.reply(msg)
+            commandsEmbed.addField("Comandos",msg,false)
+            message.reply({embeds: [commandsEmbed]})
     }
 }
