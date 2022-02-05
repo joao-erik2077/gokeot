c5689859fc3dec51fd5b8e81e144f822d2489014
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-02-05 20:39:32 -0300

    adding math command

---
+++ commands/fun/math.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: "math",
+    description: "Realiza contar matematicas",
+    category: "fun",
+    permissions: [],
+    devOnly: false,
+    run: async ({client, message, args}) => {
+        const oper = args.join(" ")
+        const result = eval(oper)
+        return message.reply(`O resultado é ${result}`)
+    }
+}
