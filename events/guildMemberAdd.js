16aa863bb9ed492fcf784c47cef3bf1fdfba72fc
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-02-05 14:03:58 -0300

    welcome image code modified

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,9 @@
-const welcomeChatId = "933731933739958303"
 const generateWelcomeImage = require("../util/generateWelcomeImage")
 
 module.exports = {
     name: "guildMemberAdd",
     run: async (bot, member) => {
+        const welcomeChatId = member.guild == "939558267414253580" ? "939563961303588924" : "933731933739958303"
         const img = await generateWelcomeImage(member)
         member.guild.channels.cache.get(welcomeChatId).send({
             content: `Seja bem vindo <@${member.id}>!!`,
