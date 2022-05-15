33648ba4fec96df85613889690722a0c265b1529
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-05-14 21:44:55 -0300

    added say command

---
+++ commands/music/say.js
@@ -0,0 +1,43 @@
+const discordTTS = require("discord-tts");
+const {
+    Client,
+    Intents
+} = require("discord.js");
+const {
+    AudioPlayer,
+    createAudioResource,
+    StreamType,
+    entersState,
+    VoiceConnectionStatus,
+    joinVoiceChannel
+} = require("@discordjs/voice");
+let voiceConnection;
+let audioPlayer=new AudioPlayer();
+
+module.exports = {
+    name: "say",
+    description: "Fala o conteúdo da mensagem",
+    category: "info",
+    permissions: [],
+    devOnly: false,
+    run: async ({client, message, args}) => {
+        const stream = discordTTS.getVoiceStream(args.join(" "), {lang: 'pt'});
+        const audioResource = createAudioResource(stream, {
+            inputType: StreamType.Arbitrary,
+            inlineVolume: true
+        });
+        if (!voiceConnection || voiceConnection?.status === VoiceConnectionStatus.Disconnected) {
+            voiceConnection = joinVoiceChannel({
+                channelId: message.member.voice.channelId,
+                guildId: message.guildId,
+                adapterCreator: message.guild.voiceAdapterCreator,
+            });
+            voiceConnection = await entersState(voiceConnection, VoiceConnectionStatus.Connecting, 5_000);
+        }
+
+        if (voiceConnection.status === VoiceConnectionStatus.Connected) {
+            voiceConnection.subscribe(audioPlayer);
+            audioPlayer.play(audioResource);
+        }
+    }
+}
